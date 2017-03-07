0506fa0640df6a15ace041c58cca9148a3305f57
Prashant Pathak <theprash@gmail.com>
2017-03-07 11:47:48 +0000

    Add Fsx module with functions to validate FSX files.

---
+++ Fake.Fsx.fsx
@@ -0,0 +1,33 @@
+module Fake
+#r "./packages/FAKE/tools/FakeLib.dll"
+open Fake
+open Fake.FscHelper
+open System.IO
+
+module Fsx =
+    let private validateScriptsWith compileF scripts =
+        let path = Path.GetTempPath() </> Path.GetTempFileName() + ".dll"
+        try
+            compileF
+                [ Out path
+                  References [ @"System.Runtime" ]
+                  FscHelper.Target TargetType.Library ]
+                scripts
+        finally DeleteFile path
+
+    /// Compiles one or more FSX script files and discards the output assembly.
+    /// Throws an exception if compilation fails.
+    /// ## Parameters
+    ///
+    ///  - `scripts` - The FSX scripts to compile.
+    let ValidateScripts scripts = validateScriptsWith Compile scripts
+
+    /// Compiles one or more FSX script files and discards the output assembly.
+    /// ## Parameters
+    ///
+    ///  - `scripts` - The FSX scripts to compile.
+    ///
+    /// ## Returns
+    ///
+    /// The exit status code of the compile process.
+    let validateScripts scripts = validateScriptsWith compile scripts
